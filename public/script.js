7bcf5287360d53aeba6c735b67e0d4bd51386913
Julian <jseale@email.sc.edu>
2023-11-11 14:28:04 -0500

    clean

---
+++ public/script.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('addButton');
+    const form = document.getElementById('form');
+    const submitButton = document.getElementById('submitButton');
+    const successMessage = document.getElementById('successMessage');
+    const errorMessage = document.getElementById('errorMessage');
+    const imageInput = document.getElementById('imageInput'); // Add this line to get the image input
+  
+    addButton.addEventListener('click', () => {
+      form.style.display = 'block';
+    });
+  
+    submitButton.addEventListener('click', async () => {
+      try {
+        const formData = new FormData(); // Use FormData to handle file uploads
+        formData.append('name', document.getElementById('name').value);
+        formData.append('year', document.getElementById('year').value);
+        formData.append('myth', document.getElementById('myth').value);
+        formData.append('culture', document.getElementById('culture').value);
+        formData.append('appearance', document.getElementById('appearance').value);
+        formData.append('distance_from_earth_miles', parseInt(document.getElementById('distance').value));
+        formData.append('img', imageInput.files[0]); // Append the image file
+  
+        const response = await fetch('http://localhost:3000/api/constellations', {
+          method: 'POST',
+          body: formData, // Use the FormData object as the body
+        });
+  
+        const result = await response.json();
+  
+        if (response.ok) {
+          successMessage.textContent = result.message;
+          successMessage.style.display = 'block';
+        } else {
+          errorMessage.textContent = result.error;
+          errorMessage.style.display = 'block';
+        }
+  
+        setTimeout(() => {
+          successMessage.style.display = 'none';
+          errorMessage.style.display = 'none';
+        }, 3000);
+  
+        fetchData();
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    });
+  
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:3000/api/constellations');
+        const data = await response.json();
+        renderData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  
+    function renderData(data) {
+      const app = document.getElementById('app');
+      app.innerHTML = '';
+  
+      data.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <h3>${item.name}</h3>
+          <p><strong>Year:</strong> ${item.year}</p>
+          <p><strong>Myth:</strong> ${item.myth}</p>
+          <p><strong>Culture:</strong> ${item.culture}</p>
+          <p><strong>Appearance:</strong> ${item.appearance}</p>
+          <p><strong>Distance from Earth:</strong> ${item.distance_from_earth_miles} miles</p>
+          <img src="${item.img}" alt="${item.name}" /> <!-- Display the image -->
+        `;
+        app.appendChild(card);
+      });
+    }
+     // Show welcome popup for 2 seconds
+  const welcomePopup = document.getElementById('welcome-popup');
+  welcomePopup.style.display = 'block';
+  setTimeout(() => {
+    welcomePopup.style.display = 'none';
+  }, 10000);
+  
+    // Initial fetch and render
+    fetchData();
+  });
+  
---
+++ server.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+const storage = multer.memoryStorage();
+const upload = multer({ dest: __dirname + "/public/images"});
+
+let constellations = [
+    {
+        name: "Orion",
+        year: "Ancient times",
+    myth: "In Greek mythology, Orion was a giant hunter. He was placed in the stars by Zeus after his death.",
+        culture: "Greek",
+        appearance: "Orion is easily recognizable by the three aligned stars forming his belt and the bright stars Betelgeuse and Rigel.",
+        distance_from_earth_miles: "1344",
+        img:"images/Orion.jpg"
+      },
+      {
+        name: "Ursa Major",
+        year: "Ancient times",
+        myth: "In Greek mythology, Ursa Major represents Callisto, a nymph who was transformed into a bear by Zeus to protect her from his jealous wife Hera.",
+        culture: "Greek",
+        appearance: "Ursa Major, also known as the Big Dipper, consists of seven bright stars that form a distinct ladle or saucepan shape.",
+        distance_from_earth_miles: "1228",
+        img:"images/Ursa.jpg"
+      },
+      {
+        name: "Crux",
+        year: "Modern",
+        myth: "No specific myth, but Crux is known as the Southern Cross and is prominent in the southern hemisphere.",
+        culture: "Southern Hemisphere cultures",
+        appearance: "Crux is a small but distinctive constellation in the shape of a cross. It is visible in the southern hemisphere.",
+        distance_from_earth_miles: "2800",
+        img:"images/Ursa.jpg"
+      },
+      {
+        name: "Draco",
+        year: "Ancient times",
+    myth: "In Greek mythology, Draco represents a dragon that guarded the golden apples in the Garden of the Hesperides.",
+        culture: "Greek",
+        appearance: "Draco is a long, winding constellation featuring a dragon. It is circumpolar, meaning it never sets from certain latitudes.",
+        distance_from_earth_miles: "3094",
+        img:"images/Draco.jpg"
+      },
+      {
+        name: "Cygnus",
+        year: "Ancient times",
+        myth: "In Greek mythology, Cygnus represents Zeus in the form of a swan. It is also associated with the story of Orpheus and his lyre.",
+        culture: "Greek",
+        appearance: "Cygnus is shaped like a cross or a swan with outstretched wings. The bright star Deneb is part of this constellation.",
+        distance_from_earth_miles: "2300",
+        img:"images/Cygnus.jpg"
+      },
+      {
+        name: "Leo",
+        year: "Ancient times",
+        myth: "In Greek mythology, Leo is associated with the Nemean Lion, a beast slain by Hercules as one of his twelve labors.",
+        culture: "Greek",
+        appearance: "Leo is a zodiacal constellation featuring a distinctive backward question mark shape known as the Sickle. It contains the bright star Regulus.",
+        distance_from_earth_miles: "2310",
+        img:"images/Leo.jpg"
+      }
+];
+
+const Joi = require('joi');
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.string().required(),
+  myth: Joi.string().required(),
+  culture: Joi.string().required(),
+  appearance: Joi.string().required(),
+  distance_from_earth_miles: Joi.number().required()
+
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.get('/api/constellations', (req, res) => {
+  res.json(constellations);
+});
+
+app.post('/api/constellations', upload.single('img'), async (req, res) => {
+    try {
+      const validatedData = schema.validate(req.body);
+  
+      if (validatedData.error) {
+        throw new Error(validatedData.error.details[0].message);
+      }
+  
+      const imagePath = req.file ? `/images/${req.file.filename}` : null;
+  
+      const newConstellation = {
+        ...validatedData.value,
+        img: imagePath,
+      };
+  
+      constellations.push(newConstellation);
+  
+      res.json({ message: 'Item added successfully' });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
